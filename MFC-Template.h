fae2b9fcd2576def004666d9ecc094e70da20537
ix84gaming <ix84gaming@gmail.com>
2025-01-14 15:27:34 +0000

    adding MFC files

---
+++ MFC-Template.h
@@ -0,0 +1,32 @@
+
+// MFC-Template.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCTemplateApp:
+// See MFC-Template.cpp for the implementation of this class
+//
+
+class CMFCTemplateApp : public CWinApp
+{
+public:
+	CMFCTemplateApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCTemplateApp theApp;
